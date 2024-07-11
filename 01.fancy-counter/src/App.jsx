0f11dff8eca7ff0a888efba207ba356823f12727
fu-jw <contact@fu-jw.com>
2024-07-11 08:09:38 +0800

    01.fancy-counter 组件化

--- 01.fancy-counter/src/App.jsx
+++ 01.fancy-counter/src/App.jsx
@@ -1,16 +1,11 @@
-import Count from "./Count";
-import OpBtn from "./OpBtn";
-import ResetBtn from "./ResetBtn";
-import Title from "./Title";
+import Card from "./Card";
+
 
 function App() {
   // 保存时 Prettier 会自动美化代码，包括添加了小括号（返回多行内容时自动添加）
   return (
     <main>
-      <Title />
-      <Count />
-      <ResetBtn />
-      <OpBtn />
+      <Card />
     </main>
   );
 }
---
+++ 01.fancy-counter/src/Card.jsx
@@ -0,0 +1,15 @@
+import Count from "./Count";
+import OpBtn from "./OpBtn";
+import ResetBtn from "./ResetBtn";
+import Title from "./Title";
+
+export default function Card() {
+  return (
+    <div>
+      <Title />
+      <Count />
+      <ResetBtn />
+      <OpBtn />
+    </div>
+  )
+}
