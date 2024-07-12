3f038efd7f0602d4dc3d9148391b39bee0d122d1
fu-jw <contact@fu-jw.com>
2024-07-12 15:28:15 +0800

    add 02.word-analytics 文本框内容校验

--- 02.word-analytics/src/H1.jsx
+++ 02.word-analytics/src/H1.jsx
@@ -1,7 +1,7 @@
 export default function H1() {
   return (
     <h1 className="first-heading">
-      Word<span className="first-heading--thin">Analytics</span>
+      字数<span className="first-heading--thin">统计</span>
     </h1>
   );
 }
--- 02.word-analytics/src/Textarea.jsx
+++ 02.word-analytics/src/Textarea.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    let newText = e.target.value;
+    // 设置之前需要检验内容，防止输入脚本等不安全内容
+    if (newText.includes("<script>")) {
+      alert("请不要输入脚本内容");
+      newText = newText.replace("<script>", "");
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <textarea
+      value={text}
+      onChange={handleChange}
+      className="textarea"
+      placeholder="请输入文本内容"
+      spellCheck="false"
+    />
+  );
 }
