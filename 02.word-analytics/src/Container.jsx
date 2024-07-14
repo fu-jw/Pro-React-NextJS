d78101789bdd3ac9bcbacca7db8ec8a979dc9c4a
fu-jw <contact@fu-jw.com>
2024-07-14 17:32:26 +0800

    update:02.word-analytics 抽取常量文件

--- 02.word-analytics/src/Container.jsx
+++ 02.word-analytics/src/Container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 export default function Container() {
   const [text, setText] = useState("");
@@ -8,8 +9,8 @@ export default function Container() {
   const stat = {
     wordNum: text.split(/\s/).filter((word) => word !== "").length,
     charNum: text.length,
-    insNum: 280 - text.length,
-    fbNum: 2200 - text.length,
+    insNum: INSTAGRAM_MAX_CHARACTERS - text.length,
+    fbNum: FACEBOOK_MAX_CHARACTERS - text.length,
   };
 
   return (
